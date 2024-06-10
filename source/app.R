f08d3c9164d5be90b039774d293fed8e4210898b
kostas-rigan <t8200145@aueb.gr>
2024-06-10 22:38:43 +0300

    [feat]: add clustering functionality

--- source/app.R
+++ source/app.R
@@ -8,8 +8,62 @@ library(dplyr)
 library(DT)
 library(stringr)
 library(lubridate)
+library(fastDummies)
+library(shinycssloaders)
+library(factoextra)
+library(cluster)
 
-data <- as_tibble(read.csv('sales_data.csv'))
+euro <- scales::label_currency(
+  largest_with_fractional = 1000000,
+  accuracy = 2,
+  prefix = '',
+  suffix = '\U20AC',
+  big.mark = '.',
+  decimal.mark = ','
+)
+
+card_visual <- function(card, title, color) {
+  return(value_box(
+    title = p(title, style = 'font-weight: bold; font-size: 24px'), 
+    value = textOutput(card),
+    theme = value_box_theme(bg = color, fg = "#F5F5F5"),
+    showcase = bsicons::bs_icon("graph-up"), showcase_layout = "left center",
+    full_screen = FALSE, fill = TRUE, height = NULL
+  ))
+}
+
+aggregator_select_input <- function(id) {
+  return(selectInput(id,
+                     label = 'Aggregator',
+                     choices = list('Sum' = 'sum',
+                                    'Average' = 'mean',
+                                    'Median' = 'median',
+                                    'Count' = 'n')))
+}
+
+metric_select_input <- function(id) {
+  return(selectInput(id,
+                     label = 'Metric',
+                     choices = list('Revenue' = 'revenue',
+                                    'Cost' = 'commissions',
+                                    'Profit' = 'profit')))
+}
+
+group_and_summarize <- function(dataframe, by_variable, input_agg_func) {
+  agg_func <- match.fun(input_agg_func)
+  
+  if (input_agg_func == 'n') {
+    grouped <- group_by(dataframe, !!by_variable) %>%
+      summarise(agg_metric = agg_func())
+  } else {
+    grouped <- group_by(dataframe, !!by_variable) %>%
+      summarise(agg_metric = agg_func(metric))
+  }
+  
+  return (grouped)
+}
+
+sales_data <- as_tibble(read.csv('sales_data.csv'))
 world <- map_data('world')
 world <- filter(world, region != 'Antarctica')
 
@@ -28,20 +82,20 @@ ui <- navbarPage(
                  width = 1/6,
                  selectInput('category', 
                              'Category', 
-                             choices = c('All', unique(data$category)), 
+                             choices = c('All', unique(sales_data$category)), 
                              selected = 'All'),
                  selectInput('is_gift', 
                              'Is Gift', 
-                             choices = c('All', unique(data$is_gift))),
+                             choices = c('All', unique(sales_data$is_gift))),
                  selectInput('publisher', 
                              'Publisher', 
-                             choices = c('All', unique(data$publisher))),
+                             choices = c('All', unique(sales_data$publisher))),
                  selectInput('narrator', 
                              'Narrator', 
-                             choices = c('All', unique(data$narrator))),
+                             choices = c('All', unique(sales_data$narrator))),
                  selectInput('country', 
                              'Country', 
-                             choices = c('All', unique(data$country))),
+                             choices = c('All', unique(sales_data$country))),
                  dateRangeInput('dates',
                                 'Dates',
                                 start = '2020-01-01',
@@ -135,6 +189,38 @@ ui <- navbarPage(
            DT::dataTableOutput('dataTable')
           ),
   
+  tabPanel('Clustering',
+           sidebarLayout(
+             sidebarPanel(
+               h1('Parameters'),
+               p(tags$i('Note: In this version all tests are conducted from k = 1 to 10')),
+               br(),
+               sliderInput('sample_size', 'Sample Size (%):', value = 0.5, min = 0, max = 1, step = 0.01),
+               selectInput('segmentation_var', label = 'Segment:', choices = c('Customers' = 'user_id', 'Orders' = 'order_id')),
+               selectInput('clustering_var', label = "By Variable:", choices = c('Category' = 'category', 'Narrator' = 'narrator')),
+               actionButton('run_tests', 'Run Test!'),
+               br(),
+               numericInput('k', 'k', value = 2, min = 2, max = 10, step = 1),
+               actionButton('run_kmeans', 'Run K-Means!')
+             ),
+             mainPanel(
+               tabsetPanel(
+                 tabPanel('Elbow',
+                          h1('Hello'),
+                          plotOutput('elbow') %>% withSpinner(color="#0dc5c1")),
+                 tabPanel('Silhouette',
+                          h1('Kalispera'),
+                          numericInput('k_sil', 'k', value = 2, min = 2, max = 10, step = 1),
+                          plotOutput('silhouette') %>% withSpinner(color="#0dc5c1")),
+                 tabPanel('Results',
+                          h1('Iaso kokla'),
+                          DT::dataTableOutput('clustering'))
+               )
+             )
+           )
+           
+           )
+  
 )
 
 server <- function(input, output, session) {
@@ -143,7 +229,7 @@ server <- function(input, output, session) {
     {
       date1 <- input$dates[1]
       date2 <- input$dates[2]
-      df <- data %>%
+      df <- sales_data %>%
         dplyr::filter(order_date >= date1 & order_date <= date2 &
                         (input$category == 'All' | input$category == category) &
                         (input$is_gift == 'All' | input$is_gift == is_gift) &
@@ -156,7 +242,7 @@ server <- function(input, output, session) {
   
   output$dataTable <- renderDataTable(
     {
-      DT::datatable(data)
+      DT::datatable(sales_data)
     }
   )
   
@@ -217,13 +303,16 @@ server <- function(input, output, session) {
   
   output$timeline <- renderPlot(
     {
-      df <- filtered()
-      grouped <- df %>%
-        group_by(as.Date(order_date)) %>%
-        summarise(rev = sum(revenue))
+      df <- filtered()[, c('order_date', input$timeline_metric)]
+      lookup = c(metric = input$timeline_metric)
       
-      colnames(grouped)[1] <- 'order_date'
-      grouped %>% ggplot(aes(x = order_date, y = rev)) + geom_line()
+      df <- rename(df, all_of(lookup))
+      df$order_date <- as.Date(df$order_date)
+      
+      by.date <- group_and_summarize(df, quo(order_date), input$timeline_agg_func)
+      
+      
+      by.date %>% ggplot(aes(x = order_date, y = agg_metric)) + geom_line()
     }
   )
   
@@ -265,56 +354,88 @@ server <- function(input, output, session) {
     }
   )
   
-}
-
-euro <- scales::label_currency(
-  largest_with_fractional = 1000000,
-  accuracy = 2,
-  prefix = '',
-  suffix = '\U20AC',
-  big.mark = '.',
-  decimal.mark = ','
-)
-
-card_visual <- function(card, title, color) {
-  return(value_box(
-    title = p(title, style = 'font-weight: bold; font-size: 24px'), 
-    value = textOutput(card),
-    theme = value_box_theme(bg = color, fg = "#F5F5F5"),
-    showcase = bsicons::bs_icon("graph-up"), showcase_layout = "left center",
-    full_screen = FALSE, fill = TRUE, height = NULL
-  ))
-}
-
-aggregator_select_input <- function(id) {
-  return(selectInput(id,
-                     label = 'Aggregator',
-                     choices = list('Sum' = 'sum',
-                                    'Average' = 'mean',
-                                    'Median' = 'median',
-                                    'Count' = 'n')))
-}
-
-metric_select_input <- function(id) {
-  return(selectInput(id,
-              label = 'Metric',
-              choices = list('Revenue' = 'revenue',
-                             'Cost' = 'commissions',
-                             'Profit' = 'profit')))
-}
-
-group_and_summarize <- function(dataframe, by_variable, input_agg_func) {
-  agg_func <- match.fun(input_agg_func)
-  
-  if (input_agg_func == 'n') {
-    grouped <- group_by(dataframe, !!by_variable) %>%
-      summarise(agg_metric = agg_func())
-  } else {
-    grouped <- group_by(dataframe, !!by_variable) %>%
-      summarise(agg_metric = agg_func(metric))
+  ## CLUSTERING
+  dummified <- function() {
+    df <- sales_data[, c(input$segmentation_var, input$clustering_var)]
+    lookup = c(seg = input$segmentation_var, cluvar = input$clustering_var)
+    
+    df <- rename(df, all_of(lookup))
+    
+    dummy <- dummy_columns(df, select_columns = 'cluvar', remove_selected_columns = T)
+    dummy <- replace(dummy, is.na(dummy), 0)
+    
+    dummy[, -1] <- scale(dummy[, -1])
+    
+    return(dummy)
   }
   
-  return (grouped)
+  cluster_test_data <- eventReactive(input$run_tests, {
+    dummy <- dummified()
+    slice_sample(dummy, prop = input$sample_size)
+  })
+  
+  cluster_run_data <- eventReactive(input$run_kmeans, dummified())
+
+  cluster_tests <- reactive(
+    {
+      req(input$run_tests)
+      sample <- cluster_test_data()
+
+      lower_k <- 1
+      higher_k <- 10
+
+      elb <- data.frame(matrix(nrow = 0, ncol = 2))
+
+      for (k in lower_k : higher_k) {
+        results <- kmeans(sample[, -1], k)
+        elb <- rbind(elb, c(k, results$tot.withinss))
+      }
+      colnames(elb) <- c('k', 'twss')
+      return(elb)
+    }
+  )
+  
+  output$elbow <- renderPlot(
+    {
+      results <- cluster_tests()
+      ggplot(results, aes(x = k, y = twss)) + geom_line() + geom_point()
+    }
+  )
+  
+  output$silhouette <- renderPlot(
+    {
+      req(input$run_tests)
+
+      sample <- cluster_test_data()
+      results <- kmeans(sample[, -1], input$k_sil)
+      sil <- silhouette(results$cluster, dist(sample[, -1]))
+      fviz_silhouette(sil)
+    }
+  )
+  
+  output$clustering <- renderDataTable(
+    {
+      req(input$run_kmeans)
+      df <- cluster_run_data()
+      results <- kmeans(df, centers = input$k)
+      df_clu <- as_tibble(data.frame(id = df$seg, cluster = results$cluster))
+
+      temp <- merge(sales_data, df_clu, by.x = input$segmentation_var, by.y = 'id', all.x=T) %>%
+        arrange(X) %>%
+        filter(duplicated(X) == FALSE)
+
+      DT::datatable(temp,
+                    extensions = 'Buttons',
+                    options = list(scrollX = TRUE,
+                                   paging = TRUE,
+                                   searching = TRUE,
+                                   fixedColumns = TRUE,
+                                   autoWidth = TRUE,
+                                   ordering = TRUE,
+                                   dom = 'tB',
+                                   buttons = c('copy', 'csv', 'excel')))
+    }
+  )
 }
 
 shinyApp(ui, server)
--- source/tmp.R
+++ source/tmp.R
@@ -2,115 +2,53 @@ library(ggplot2)
 library(dplyr)
 library(lubridate)
 library(forcats)
+library(fastDummies)
+library(factoextra)
 
 data <- as_tibble(read.csv('sales_data.csv'))
 data
 colnames(data)
 
-grouped <- data %>%
-  group_by(is_gift) %>%
-  summarise(rev = sum(revenue))
-grouped
-ggplot(grouped, aes(x = as.factor(is_gift), y = rev)) + geom_col()
-
-grouped <- data %>%
-  group_by(as.Date(order_date)) %>%
-  summarise(rev = sum(revenue))
-
-colnames(grouped)[1] <- 'order_date'
-
-ggplot(grouped, aes(x = order_date, y = rev)) + geom_line()
-
-total_revenue <- sum(data$revenue)
-plot_data <- data.frame(total_revenue)
-
-euro <- scales::label_currency(
-  largest_with_fractional = 1000000,
-  accuracy = 2,
-  prefix = '',
-  suffix = '\U20AC',
-  big.mark = '.',
-  decimal.mark = ','
-)
-
-grouped <- data %>%
-  group_by(category) %>%
-  summarise(rev = sum(revenue))
-
-grouped <- grouped[!is.na(grouped[, 1]), ]
-
-ggplot(grouped, aes(x = rev, y = fct_reorder(factor(category), rev, na.rm = F))) + geom_col()
-
-unique(grouped$category)
-
-world <- map_data('world')
-head(world)
-
-greece <- filter(world, region == 'Greece')
-ggplot(greece, aes(x = long, y = lat, group = group, fill = subregion)) + geom_polygon() + coord_quickmap()
-ggplot(world, aes(x = long, y = lat, group = group)) + 
-  geom_polygon() + 
-  coord_quickmap() +
-  theme_void()
-
-by.country <- group_by(data, country) %>%
-  summarise(revenue = sum(revenue),
-            cost = sum(commissions),
-            profit = sum(profit),
-            quantity = sum(quantity),
-            orders = n())
-by.country
-world.wo.antarctica <- world %>% 
-  filter(region != 'Antarctica')
-country.agg <- merge(world.wo.antarctica, by.country, by.x = 'region', by.y = 'country', all.x=T)
-country.agg
-
-ggplot(country.agg, aes(x = long, y = lat, group = group)) + 
-  geom_polygon() + 
-  coord_quickmap() +
-  theme_void()
-
-arrange(country.agg, region, group, order) %>%
-  ggplot(aes(x = long, y = lat, group = group, fill = revenue)) + 
-  geom_polygon() + 
-  coord_quickmap() +
-  theme_void() +
-  theme(legend.position = 'none')
-
-
-date_data <- data[, c('order_date', 'revenue', 'commissions', 'profit')]
-date_data$day_of_week <-wday(date_data$order_date, label = T, abbr = F)
-date_data$month <- month(as.Date(date_data$order_date), label = T, abbr = F)
-date_data$hour_of_day <- ymd_hms(date_data$order_date) %>%
-  hour()
-
-f1 <- match.fun('sum')
-f2 <- match.fun('mean')
-date_data
-grouped_date <- date_data %>%
-  group_by(hour_of_day) %>%
-  summarise(revenue = f2(revenue), n = n())
-ggplot(grouped_date, aes(x = hour_of_day, y = n)) + geom_col()
-
 data
-col = 'price'
-data[, col]
+dummy <- dummy_columns(data, select_columns = 'category', remove_selected_columns = T)
+dummy <- select(dummy, order_id, starts_with('category'))
+orders <- dummy %>% group_by(order_id) %>% summarize_all(sum)
 
-match.fun('wday')
+dummy <- dummy_columns(data, select_columns = 'category', remove_selected_columns = T)
+dummy <- select(dummy, user_id, starts_with('category'))
+users <- dummy %>% group_by(user_id) %>% summarize_all(sum)
 
-group_and_summarize <- function(dataframe, by_variable, input_agg_func, metric) {
-  agg_func <- match.fun(input_agg_func)
 
-  if (input_agg_func == 'n') {
-    grouped <- group_by(dataframe, !!by_variable) %>%
-      summarise(agg_metric = agg_func())
-  } else {
-    grouped <- group_by(dataframe, !!by_variable) %>%
-      summarise(agg_metric = agg_func(!!metric))
-  }
-  
-  return (grouped)
+
+users <- replace(users, is.na(users), 0)
+users[, -1] <- scale(users[, -1])
+users[, -1]
+results<-kmeans(users[, -1], 3)
+results$cluster
+
+lower_k <- 1
+higher_k <- 10
+
+sample <- slice_sample(users, prop = 0.1)
+
+elb <- data.frame(matrix(nrow = 0, ncol = 2))
+
+
+for (k in lower_k : higher_k) {
+  results <- kmeans(sample[, -1], k)
+  elb <- rbind(elb, c(k, results$tot.withinss))
 }
+colnames(elb) <- c('k', 'twss')
+elb
+ggplot(elb, aes(x = k, y = twss)) + geom_line() + geom_point()
 
-data
-group_and_summarize(data, quo(country), 'sum', quo(profit))
+require(cluster)
+sil <- silhouette(results$cluster, dist(sample[, -1]))
+fviz_silhouette(sil)
+sil
+
+lst = list()
+for (i in 1:10) {
+  lst <- append(lst, i)
+}
+lst
